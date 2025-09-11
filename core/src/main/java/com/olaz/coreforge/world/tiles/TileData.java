4fdab2081dff9efc3a8d28347b1255c65f76e087
phuocn0302 <phuocnguyen230302@gmail.com>
2025-09-11 23:44:28 +0700

    feat: add randomness to texture tile json data file now has textures array with weight remove texture border

--- core/src/main/java/com/olaz/coreforge/world/tiles/TileData.java
+++ core/src/main/java/com/olaz/coreforge/world/tiles/TileData.java
@@ -1,9 +1,16 @@
 package com.olaz.coreforge.world.tiles;
 
+import java.util.List;
+
 /**
  * Data model for tiles JSON file
  */
 public class TileData {
     public String type;
-    public String texture;
+    public List<TextureEntry> textures;
+
+    public static class TextureEntry {
+        public String path;
+        public int weight = 1;
+    }
 }
--- core/src/main/java/com/olaz/coreforge/world/tiles/TileFactory.java
+++ core/src/main/java/com/olaz/coreforge/world/tiles/TileFactory.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.math.MathUtils;
 import com.olaz.coreforge.world.tiles.types.FluidTile;
 import com.olaz.coreforge.world.tiles.types.GroundTile;
 import com.olaz.coreforge.world.tiles.types.MineralTile;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TileFactory {
@@ -34,8 +36,26 @@ public class TileFactory {
             throw new IllegalArgumentException("No tile definition found for id: " + id);
         }
 
+        List<TileData.TextureEntry> textures = data.textures;
+
+        int totalWeight = 0;
+        for (TileData.TextureEntry t : textures) {
+            totalWeight += t.weight;
+        }
+
+        int r = MathUtils.random(totalWeight - 1);
+        String texturePath = null;
+
+        for (TileData.TextureEntry t : textures) {
+            if (r < t.weight) {
+                texturePath = t.path;
+                break;
+            }
+            r -= t.weight;
+        }
+
         Texture texture = textureCache.computeIfAbsent(
-            data.texture,
+            texturePath,
             path -> new Texture(Gdx.files.internal(path))
         );
 
